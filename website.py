23fa5caed073742275aabbe156b659e99a12df46
pranaysahare <110258055+pranaysahare1206@users.noreply.github.com>
2023-08-17 14:50:01 +0530

    website.py

--- website.py
+++ website.py
@@ -4,8 +4,9 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 from streamlit_lottie import st_lottie
-st.set_page_config(page_title="Pranay.Sahare_Website",page_icon=":loop:",layout="wide")
 st.config(hide_streamlit_logo=True)
+st.set_page_config(page_title="Pranay.Sahare_Website",page_icon=":loop:",layout="wide")
+
 #--------load assets------------------------------------------------------------------------
 
 
